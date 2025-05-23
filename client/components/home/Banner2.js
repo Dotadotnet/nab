2b9dd94e1757da5915cedf2d7a759a4e59a49c0c
Dotadotnet <Dotadotnet@outlook.com>
2025-05-24 00:43:27 +0330

    delete un limit image from banner2

--- client/components/home/Banner2.js
+++ client/components/home/Banner2.js
@@ -9,10 +9,10 @@ const Banner2 = ({}) => {
   return (
     <Container >
       <div
-        className="bg-yellow-100  dark:bg-red-600 h-full pt-8 w-full dark:text-gray-100 rounded-primary gap-16 flex md:flex-row flex-col gap-y-0  px-8  "
+        className="bg-yellow-100  dark:bg-red-500 h-full pt-8 w-full dark:text-gray-100 rounded-primary gap-16 flex md:flex-row flex-col gap-y-0  px-8  "
         style={{ backgroundImage: "url(/assets/home/banner/dots.svg)" }}
       >
-        <div className="w-96 h-fit order-2 md:order-1">
+        <div className="w-fit h-fit order-2 md:order-1">
           <Image
             src="/assets/home/banner/kid.webp"
             alt="model"
