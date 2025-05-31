fdfa9c0413e853e3808c09e663d967bf63e97fca
Dotadotnet <Dotadotnet@outlook.com>
2025-06-01 00:11:10 +0330

    change alt page

--- client/components/shared/languageSwitch/page.js
+++ client/components/shared/languageSwitch/page.js
@@ -4,6 +4,8 @@ import { useLocale } from "next-intl";
 import language from "@/app/language";
 import { usePathname } from "next/navigation";
 import OutsideClick from "../outsideClick/OutsideClick";
+import Image from "next/image";
+
 const LanguageSwitcher = () => {
   const [isOpen, setIsOpen] = useState(false);
   const t = useLocale();
@@ -11,36 +13,40 @@ const LanguageSwitcher = () => {
   const lang_now = class_lang.getInfo();
   const langs = class_lang.info;
   const langs_result = [];
+
+  const pathname = usePathname().split("/");
+
   langs.forEach((lang) => {
-    const pathname = usePathname().split("/");
-    if (t == "fa") {
-      pathname.unshift(lang.lang)
-            pathname.unshift('')
+    let newPath = [...pathname];
+    if (t === "fa") {
+      newPath.unshift(lang.lang);
+      newPath.unshift("");
     } else {
-      pathname[1] = lang.lang;
+      newPath[1] = lang.lang;
     }
     langs_result.push({
       lang: lang.lang,
       img: lang.img,
       name: lang.name,
-      link:   pathname.join("/").replace("//", "/"),
+      link: newPath.join("/").replace("//", "/"),
       loc: lang.loc,
       dir: lang.dir
     });
-
   });
-  
+
   return (
     <div className="relative">
       <button
         onClick={() => setIsOpen(!isOpen)}
         className=" rounded-secondary flex px-4 py-1 md:p-0 items-center justify-center gap-x-2 bg-slate-100 dark:bg-slate-900 hover:bg-slate-200 dark:hover:bg-slate-700 transition-colors md:w-10 md:h-10"
       >
-        <span className="md:hidden flex ">{lang_now.name}</span>
-        <img
+        <span className="md:hidden flex">{lang_now.name}</span>
+        <Image
           src={lang_now.img}
           alt={lang_now.name}
-          className="w-7 h-7 rounded-full "
+          width={20}
+          height={20}
+          className="rounded-full w-7 h-7 "
         />
       </button>
 
@@ -56,12 +62,16 @@ const LanguageSwitcher = () => {
               href={lang.link}
               key={lang.lang}
               className="flex items-center gap-2 p-2 hover:bg-gray-100 dark:hover:bg-gray-700 rounded w-full text-right"
+              aria-current={lang.lang === t ? "true" : undefined}
             >
-              <img
+              <Image
                 src={lang.img}
                 alt={lang.name}
-                className="w-5 h-5 rounded-full"
+                width={20}
+                height={20}
+                className="rounded-full w-5 h-5"
               />
+
               <span className="text-sm font-vazir">{lang.name}</span>
             </a>
           ))}
